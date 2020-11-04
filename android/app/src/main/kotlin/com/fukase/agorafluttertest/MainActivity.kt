02939afda1430b0407168b06fc7d26a3b7f06a33
深瀬 貴将 <fukase@arsaga.jp>
2020-11-04 12:12:00 +0900

    copied files from Agora-Flutter-Quickstart

--- android/app/src/main/kotlin/com/fukase/agorafluttertest/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.fukase.agorafluttertest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = "3eb868c642c741b19bfaaab66031c45a";
+const Token = "3eb868c642c741b19bfaaab66031c45a";
